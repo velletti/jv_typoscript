dedb405c992ce7bf76b582bb5288601107cae50b
jvelletti <jvelletti@allplan.com>
2025-02-28 16:32:10 +0100

    force to be called with no_cache=1 as a cached pages does not have typoscript settings as array

--- Classes/Middleware/JvTyposcript.php
+++ Classes/Middleware/JvTyposcript.php
@@ -8,11 +8,12 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+use TYPO3\CMS\Core\Http\RedirectResponse;
 use TYPO3\CMS\Core\Http\Response;
 use TYPO3\CMS\Core\Http\Stream;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 /**
  * Class Typoscript
  * @package JVelletti\JvTyposcript\Middleware
@@ -33,54 +34,67 @@ class JvTyposcript implements MiddlewareInterface
 
         $_gp = $request->getQueryParams();
         if( is_array($_gp) && key_exists("tx_jvtyposcript" ,$_gp ) ) {
-            $this->getTypoScript($request , $_gp['tx_jvtyposcript']) ;
+            if( array_key_exists("no_cache" , $_gp )) {
+                $this->getTypoScript($request , $_gp['tx_jvtyposcript']) ;
+            } else {
+                $url = GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL')  . "&no_cache=1" ;
+                return   new RedirectResponse(
+                     GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL') . "&no_cache=1" , 307 ,
+                     ['Pragma' => 'no-cache' , 'Cache-Control' => 'no-cache' , 'Expires' => '0' ]
+                ) ;
+
+            }
+
         }
 
         return $handler->handle($request);
     }
 
     private function getTypoScript($request , $extKey = "all")
+
     {
+
         $frontendTs = $request->getAttribute('frontend.typoscript');
         if ( $frontendTs->hasSetup() ) {
             $ts = $frontendTs->getSetupArray();
+            if ( ! array_key_exists('plugin.' ,  $ts )) {
+                return ;
+            }
+            $ts['plugin']  = self::removeDotsFromTypoScriptArray($ts['plugin.'] );
         } else {
-            return ;
+            // Retrieve the site configuration via the new Site API
+            // Fetch the Pages TSconfig for the given PID.
+            if ( ! array_key_exists('plugin' ,  $ts )) {
+                return ;
+            }
         }
 
-        if ( ! array_key_exists('plugin.' ,  $ts )) {
-            return ;
-        }
         $configuration = EmConfigurationUtility::getEmConf();
         if( !array_key_exists('allowed' , $configuration)) {
             return ;
         }
+
         $configuration = GeneralUtility::trimExplode( "," ,$configuration['allowed']);
-
-        if( $extKey == "all") {
-            $ts = self::removeDotsFromTypoScriptArray($ts['plugin.']);
-        } else {
-            if ( ! array_key_exists($extKey . '.' ,  $ts['plugin.'] )) {
-                return ;
-            }
-            $tempTs = self::removeDotsFromTypoScriptArray($ts['plugin.'][$extKey . '.']);
-            $ts = [] ;
-            $ts[$extKey] = $tempTs ;
+        if( !is_array($configuration) || count($configuration) == 0 ) {
+            return ;
         }
-
-        $result =[] ;
-        if ( is_array($configuration) && count($configuration) > 0 ) {
-            foreach ( $ts as $extension => $value ) {
-                foreach ( $configuration as $allowed ) {
-                    if ( strpos( $extension , $allowed ) > -1 ) {
-                        $result[$extension] = $value ;
-                    }
+        $result = [] ;
+        foreach ( $ts['plugin'] as $extension => $value ) {
+            foreach ( $configuration as $allowed ) {
+                if ( strpos( $extension , $allowed ) > -1 ) {
+                    $result[$extension] = $value ;
                 }
             }
         }
-        if ( $extKey != "all") {
-            $result = $result[$extKey] ;
-        }
+         if( $extKey != "all" ) {
+               if( array_key_exists($extKey , $result) ) {
+                  $result = [$extKey => $result[$extKey]];
+               } else {
+                  $result = [] ;
+               }
+         }
+
+
 
         $jsonOutput = json_encode($result);
         header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
@@ -92,6 +106,27 @@ class JvTyposcript implements MiddlewareInterface
         header('Content-Transfer-Encoding: 8bit');
         echo $jsonOutput;
         die();
+    }
+    private static function convertFlatToArray(?array $flat ) {
+        if (is_array($flat)) {
+            $ts = [];
+            foreach ($flat as $key => $value) {
+                $keys = explode('.' , $key);
+                $temp = &$ts ;
+                foreach ($keys as $k) {
+                    if (is_array($temp)) {
+                        if (!array_key_exists($k, $temp)) {
+                            $temp[$k] = [];
+                        }
+                        $temp = &$temp[$k];
+                    }
+
+                }
+                $temp = $value ;
+            }
+            return $ts ;
+        }
+        return false ;
     }
 
     /**
--- Documentation/ChangeLog/Index.rst
+++ Documentation/ChangeLog/Index.rst
@@ -10,6 +10,7 @@
 
 ChangeLog
 =========
+    version 12.4.9: Redirect request with no_cache=1 parameter to the same URL if the page is cached (cached Pages has not TYPO3 Frontend TypoScript setup in request)
     version 12.4.8: fix trainling colon in composer json
     version 11.4.7: do not throw 500 error if frontend.typoscript  does not have typoscript setup because of cached content (exception in Core extension)
     Version 11.4.4: Working with PHP 8.2 and tested with TYPO3 Version 12.4.x
